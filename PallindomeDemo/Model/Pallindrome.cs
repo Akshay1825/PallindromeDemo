1e241a94c39dbf333bc22e50645e399094b9ae0e
Akshay Utekar <akshayutekar1825@gmail.com>
2024-09-25 22:48:02 +0530

    Add project files.

---
+++ PallindomeDemo/Model/Pallindrome.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PallindomeDemo.Model
+{
+    internal class Pallindrome
+    {
+        public bool IsPallindrome(int num)
+        {
+            int temp = num;
+            int reverse = 0;
+
+            while (temp > 0)
+            {
+                int remainder = temp % 10;
+                reverse = (reverse * 10) + remainder;
+                temp /= 10;
+            }
+            return reverse == num;
+        }
+    }
+}
---
+++ PallindomeDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using PallindomeDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PallindomeDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pallindrome pallindrome = new Pallindrome();
+
+            Console.WriteLine("Enter a number :");
+            int number = int.Parse(Console.ReadLine());
+
+            string answer = pallindrome.IsPallindrome(number)
+                            ? $"{number} is a Palindrome number."
+                            : $"{number} is not a Palindrome number.";
+
+            Console.WriteLine(answer);
+        }
+    }
+}
